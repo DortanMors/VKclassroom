7fb0c90ad260de43db51256b158bbce179a6e536
DortanMors <dortan_mors@mail.ru>
2020-07-06 10:36:46 +0400

    Create BusinessCard

---
+++ src/card/BusinessCard.js
@@ -0,0 +1,12 @@
+// title : string
+// photo : string
+// info  : object
+class BusinessCard {
+    constructor(title, photo, info){
+        this.title = title;
+        this.photo = photo;
+        this.info = info;
+    }
+};
+
+export default BusinessCard;
