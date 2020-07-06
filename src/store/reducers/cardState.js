08a8e64470be532513bf6af4e345d59a25900781
DortanMors <dortan_mors@mail.ru>
2020-07-06 12:51:50 +0400

    default values of businessCard

--- src/store/reducers/cardState.js
+++ src/store/reducers/cardState.js
@@ -1,5 +1,14 @@
 const initialState = {
-  cards: []
+  cards: [
+    {
+      title: 'Mors Corp.',
+      photo: 'https://sun9-38.userapi.com/c206816/v206816747/155343/Jh6GEcRqGCk.jpg',
+      info: {
+        distance: 1
+      }
+    }
+  ],
+  gone: new Set()
 }
   
 export default function cardState(state = initialState, action) {
