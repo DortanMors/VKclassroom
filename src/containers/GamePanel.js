27127aaa0b0462137f318670daa1ee875bc0cd40
DortanMors <dortan_mors@mail.ru>
2020-07-07 23:25:32 +0400

    Revert "Correct styles"
    This reverts commit 934d8acd6dcb841bf05ad135ded98b88f3e72128.

--- src/containers/GamePanel.js
+++ src/containers/GamePanel.js
@@ -6,17 +6,17 @@ import { useGesture } from "react-with-gesture";
 import BusinessCardContainer from './BusinessCardContainer';
 import { setGone } from '../store/vk/actions';
 
-import '../styles/GamePanel.css';
+//import '../styles/GamePanel.css';
 import { Panel, PanelHeader } from '@vkontakte/vkui';
 
 const to = i => ({
 	x: 0,
-	y: 0,
+	y: i * -10,
 	scale: 1,
 	rot: -10 + Math.random() * 20,
 	delay: i * 100
   });
-  const from = i => ({ x: 0, rot: 0, scale: 1.5, y: -1000 });
+  const from = i => ({ rot: 0, scale: 1.5, y: -1000 });
   
   const trans = (r, s) =>
 	`perspective(1500px) rotateX(30deg) rotateY(${r /
@@ -44,17 +44,13 @@ function GamePanel(props) {
 		  if (!down && trigger){
 			const new_gone = props.gone;
 			new_gone.add(index);
-			if (dir===1){
-				console.log('Another one to the right!');
-			}
 			props.dispatch(setGone(new_gone));
 		  }
 	
 		  set(i => {
 			if (index !== i) return;
-
 			const isGone = props.gone.has(index);
-
+	
 			const x = isGone ? (200 + window.innerWidth) * dir : down ? xDelta : 0;
 	
 			const rot = xDelta / 100 + (isGone ? dir * 10 * velocity : 0);
@@ -70,7 +66,7 @@ function GamePanel(props) {
 		  });
 	
 		  if (!down && props.gone.size === props.cards.length)
-			setTimeout(() => props.dispatch(setGone(new Set())) || set(i => to(i)), 600);
+			setTimeout(() => props.gone.clear() || set(i => to(i)), 600);
 		}
 	  );
 
@@ -79,21 +75,18 @@ function GamePanel(props) {
 			<PanelHeader>
 				VK classroom
 			</PanelHeader>
-			{springs_props.map(( springs_prop, i) => {
-				console.log(springs_prop);
-				return(
+			{springs_props.map(({ x, y, rot, scale }, i) => (
 			<BusinessCardContainer
 				i={i}
-				x={springs_prop.x}
-				y={springs_prop.y}
-				rot={springs_prop.rot}
-				scale={springs_prop.scale}
+				x={x}
+				y={y}
+				rot={rot}
+				scale={scale}
 				trans={trans}
 				data={props.cards[i]}
 				bind={bind}
-				key={i}
 			/>
-			)})}
+			))}
 		</Panel>
 	);
 }
