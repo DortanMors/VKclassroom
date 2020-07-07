b893e89fc0947f6b45ed818c48227b3324369c44
DortanMors <dortan_mors@mail.ru>
2020-07-07 07:56:38 +0400

    Implemented cardState

--- src/store/reducers/cardState.js
+++ src/store/reducers/cardState.js
@@ -2,9 +2,10 @@ const initialState = {
   cards: [
     {
       title: 'Mors Corp.',
-      photo: 'https://sun9-38.userapi.com/c206816/v206816747/155343/Jh6GEcRqGCk.jpg',
+      pics: ['https://sun9-38.userapi.com/c206816/v206816747/155343/Jh6GEcRqGCk.jpg'],
       info: {
-        distance: 1
+        distance: 1,
+        text: 'Chebureki'
       }
     }
   ],
@@ -18,11 +19,20 @@ export default function cardState(state = initialState, action) {
         ...state,
         cards: action.payload
       }
+    case 'SET_GONE':
+      return {
+        ...state,
+        gone: action.payload
+      }
     default:
       return state;
   }
 }
 
 export function getCards(state){
-  return state.gameState.cards
+  return state.cardState.cards
+}
+
+export function getGone(state){
+  return state.cardState.gone
 }
