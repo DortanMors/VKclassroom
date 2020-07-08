7a288e0bd77f7414949f264ecbf1a8b5048b2404
DortanMors <dortan_mors@mail.ru>
2020-07-08 19:26:04 +0400

    Cards moved into Deck

--- src/containers/GamePanel.js
+++ src/containers/GamePanel.js
@@ -1,108 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {connect} from 'react-redux';
-
-import { useSprings } from "react-spring";
-import { useGesture } from "react-with-gesture";
-import BusinessCardContainer from './BusinessCardContainer';
-import { setGone } from '../store/vk/actions';
+import { RouteNode } from 'react-router5';
 
 import '../styles/GamePanel.css';
-import { Panel, PanelHeader } from '@vkontakte/vkui';
+import { Panel, PanelHeader} from '@vkontakte/vkui';
 
-const to = i => ({
-	x: 0,
-	y: 0,
-	scale: 1,
-	rot: -10 + Math.random() * 20,
-	delay: i * 100
-  });
-  const from = i => ({ x:0, rot: 0, scale: 1.5, y: -1000 });
-  
-  const trans = (r, s) =>
-	`perspective(1500px) rotateX(30deg) rotateY(${r /
-	  10}deg) rotateZ(${r}deg) scale(${s})`;  
+import { getCards, getGone, getDiscarded } from '../store/reducers/cardState';
+import Deck from './Deck';
 
-function GamePanel(props) {
-	const [springs_props, set] = useSprings(props.cards.length, i => ({
-		...to(i),
-		from: from(i)
-	  }));
-	
-	  const bind = useGesture(
-		({
-		  args: [index],
-		  down,
-		  delta: [xDelta],
-		  distance,
-		  direction: [xDir],
-		  velocity
-		}) => {
-		  const trigger = velocity > 0.2;
-	
-		  const dir = xDir < 0 ? -1 : 1;
-	
-		  if (!down && trigger){
-			const new_gone = props.gone;
-			new_gone.add(index);
-			if (dir===1){
-				console.log('Another one to the right!'); // TODO сюда засунуть логику
-			}
-			props.dispatch(setGone(new_gone));
-		  }
-	
-		  set(i => {
-			if (index !== i) return;
-			const isGone = props.gone.has(index);
-	
-			const x = isGone ? /*(200 + window.innerWidth)*/200 * dir : down ? xDelta : 0;
-	
-			const rot = xDelta / 100 + (isGone ? dir * 10 * velocity : 0);
-	
-			const scale = down ? 1.1 : 1;
-			return {
-			  x,
-			  rot,
-			  scale,
-			  delay: undefined,
-			  config: { friction: 50, tension: down ? 800 : isGone ? 200 : 500 }
-			};
-		  });
-	
-		  if (!down && props.gone.size === props.cards.length)
-			setTimeout(() => props.dispatch(setGone(new Set())) || set(i => to(i)), 600);
-		}
-	  );
+class GamePanel extends Component {
 
-	return (
-		<Panel id={props.id} centered={true}>
-			<PanelHeader>
-				VK classroom
-			</PanelHeader>
-			{springs_props.map((springs_prop, i) => {
-				console.log(springs_prop)
-				if(props.gone.has(i)) return 0;
-				return(
-					<BusinessCardContainer
-						i={i}
-						x={springs_prop.x}
-						y={springs_prop.y}
-						rot={springs_prop.rot}
-						scale={springs_prop.scale}
-						trans={trans}
-						data={props.cards[i]}
-						bind={bind}
-						key={i}
-						gone={props.gone}
-					/>
-				);
-			})}
-		</Panel>
-	);
+	render(){
+		return (
+			<Panel id={this.props.id} centered={true}>
+				<PanelHeader>
+					VK classroom
+				</PanelHeader>
+				<Deck
+					id={this.props.id}
+                    router={this.props.router}
+					gone={this.props.gone}
+					cards={this.props.cards}
+					discarded={this.props.discarded}
+				/>
+			</Panel>
+		);
+	}
 }
 
 
 function mapStateToProps(state) {
-    return {};
+    return {
+        gone:  getGone(state),
+		cards: getCards(state),
+		discarded: getDiscarded(state)
+	};
 }
 
-export default connect(mapStateToProps)(GamePanel);
+
+export default connect(mapStateToProps)(
+    (props) => (
+        <RouteNode nodeName="game">
+            {({ route }) => <GamePanel route={route} {...props}/>}
+        </RouteNode>
+    )
+);
