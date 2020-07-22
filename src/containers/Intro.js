a94c5401f1abb5d29f53ee99191730ab204a94b6
DortanMors <dortan_mors@mail.ru>
2020-07-23 03:29:03 +0400

    implement auto navigate to home from intro

--- src/containers/Intro.js
+++ src/containers/Intro.js
@@ -1,54 +1,36 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
-import { Panel, PanelHeader, Avatar, FixedLayout, Button, Div, Snackbar } from '@vkontakte/vkui'
+import { Panel, PanelHeader, FixedLayout, Button, Div } from '@vkontakte/vkui'
 import { setStorageSawIntro } from '../store/vk/actions'
 
 class Intro extends Component {
 
 	render() {
-		return (
-			<Panel id={this.props.id} centered={true}>
-			<PanelHeader>
-				Fun Finder
-			</PanelHeader>
-				<Fragment>
-					<FixedLayout vertical='top'>
+		return (!this.props.popout &&
+			<Panel id={this.props.id}>
+				<PanelHeader>Fun Finder</PanelHeader>
+					<Div>
 						<h3>
 							Сервис предназначен для выбора досуга. Переключитесь на раздел "Погнали" и начните поиск. 
 							Вам будут предложены карточки мест для посещения. Смахните карточку понравившегося варианта вправо, он отобразится в разделе "Главная"
 						</h3>
-					</FixedLayout>
-					<FixedLayout vertical='bottom'>
-						<h5>Внимание: сервис получает данные из сторонних источников, что может использовать дополнительный траффик.</h5>
+					</Div>
+					<Div>
+						<Button mode='primary' size='xl' onClick={() => {
+								try{
+									this.props.dispatch(setStorageSawIntro(this.props.storageKeys));
+									this.props.router.navigate('home');
+								}catch(error){console.log(error)}
+							}}
+						>Продолжить</Button>
+					</Div>
+					<FixedLayout vertical="bottom">
 						<Div>
-							<Button mode='primary' size='xl' onClick={() => {
-									try{
-										this.props.dispatch(setStorageSawIntro(this.props.storageKeys));
-										this.props.router.navigate('home');
-									}catch(error){
-										this.props.dispatch({
-											type: 'SET_SNACKBAR',
-											payload: <Snackbar 
-														layout='vertical' 
-														onClose={()=> this.props.dispatch({ type: 'SET_SNACKBAR', payload: null })}
-														before={<Avatar size={24} style={{backgroundColor: 'var(--dynamic-red)'}}>
-																<Icon24Error fill='#fff' width='14' height='14'/>
-															</Avatar>}
-														duration={666}>
-														Загрузка в Storage недоступна
-													</Snackbar>
-										})
-									}
-									
-								}}
-							>
-								Продолжить
-							</Button>
-							<h4>Информация получена с помощью Google</h4>
+							<h5>Внимание: сервис получает данные из сторонних источников, что может использовать дополнительный траффик.</h5>
+							<h5>Информация получена с помощью Google</h5>
 						</Div>
 					</FixedLayout>
-				</Fragment>
-			{this.props.snackbar}
+				{this.props.snackbar}
 		</Panel>
 		);
 	}
